ba368689556f297fa7d3ddc561dc6c82bca8d2c0
Serge Vakulenko <serge.vakulenko@gmail.com>
2025-10-30 18:34:58 -0700

    Next stage of source reorganization.

--- file_io.cpp
+++ file_io.cpp
@@ -8,35 +8,6 @@
 #include "editor.h"
 
 //
-// Load line from workspace into current line buffer.
-//
-void Editor::get_line(int lno)
-{
-    if (current_line_no_ == lno) {
-        // We already have this line.
-        return;
-    }
-
-    // Save any unsaved modifications
-    put_line();
-
-    current_line_          = wksp_->read_line(lno);
-    current_line_no_       = lno;
-    current_line_modified_ = false;
-}
-
-//
-// Write current line buffer back to workspace if modified.
-//
-void Editor::put_line()
-{
-    if (current_line_modified_ && current_line_no_ >= 0) {
-        wksp_->put_line(current_line_no_, current_line_);
-    }
-    current_line_modified_ = false;
-}
-
-//
 // Open initial file from command line arguments.
 //
 void Editor::open_initial(int argc, char **argv)
